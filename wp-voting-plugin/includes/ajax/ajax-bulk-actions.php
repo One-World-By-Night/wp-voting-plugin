7a76413761f65bfbef30f0d703ad17a623e33808
Greg Hacke <greghacke@rskgroup.org>
2025-07-24 16:25:18 -0400

    bulk

---
+++ wp-voting-plugin/includes/ajax/ajax-bulk-actions.php
@@ -0,0 +1,10 @@
+<?php
+
+/** File: includes/ajax/ajax-bulk-actions.php
+ * Text Domain: wp-voting-plugin
+ * @version 2.0.0
+ * @author greghacke
+ * Function: Display and manage all votes in admin
+ */
+
+defined('ABSPATH') || exit;
