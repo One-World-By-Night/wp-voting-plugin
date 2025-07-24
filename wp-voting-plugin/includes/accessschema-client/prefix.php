37ee9567d01824e268d8adaff41a035a89fc5f3d
Greg Hacke <greghacke@rskgroup.org>
2025-07-24 12:03:40 -0400

    Fixing prefix

--- wp-voting-plugin/includes/accessschema-client/prefix.php
+++ wp-voting-plugin/includes/accessschema-client/prefix.php
@@ -6,7 +6,7 @@
  */
 
 // Set the unique prefix for this plugin instance
-define('ASC_PREFIX', 'wpvp');
+define('ASC_PREFIX', 'WPVP');
 
 // Set the display label for admin menus
 define('ASC_LABEL', 'WP Voting Plugin');
