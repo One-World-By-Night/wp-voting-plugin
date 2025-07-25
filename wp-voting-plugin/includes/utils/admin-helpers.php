b5b6f1cbabb5d5781476abccdef906e3e3aa65b7
Greg Hacke <greghacke@rskgroup.org>
2025-07-25 12:56:43 -0400

    Updates

--- wp-voting-plugin/includes/utils/admin-helpers.php
+++ wp-voting-plugin/includes/utils/admin-helpers.php
@@ -7,3 +7,48 @@
  */
 
 defined('ABSPATH') || exit;
+
+function wpvp_votes_add_query_arg($args = array()) {
+    $base_url = admin_url('edit.php?post_type=vote');
+    return add_query_arg($args, $base_url);
+}
+
+function wpvp_votes_remove_query_arg($key) {
+    $base_url = admin_url('edit.php?post_type=vote');
+    return remove_query_arg($key, $base_url);
+}
+
+function wpvp_get_current_filters() {
+    $filters = array();
+    if (isset($_GET['s'])) {
+        $filters['search'] = sanitize_text_field($_GET['s']);
+    }
+    if (isset($_GET['status'])) {
+        $filters['status'] = sanitize_text_field($_GET['status']);
+    }
+    if (isset($_GET['type'])) {
+        $filters['type'] = sanitize_text_field($_GET['type']);
+    }
+    return $filters;
+}
+
+function wpvp_get_pagination_args($total_items, $per_page = 20) {
+    $current_page = max(1, get_query_var('paged', 1));
+    $total_pages = ceil($total_items / $per_page);
+
+    return array(
+        'total_items' => $total_items,
+        'per_page' => $per_page,
+        'current_page' => $current_page,
+        'total_pages' => $total_pages,
+        'base' => wpvp_votes_add_query_arg(array('paged' => '%#%')),
+        'format' => '?paged=%#%',
+    );
+}
+
+function wpvp_process_search_query($query) {
+    if (empty($query)) {
+        return '';
+    }
+    return sanitize_text_field($query);
+}
--- wp-voting-plugin/includes/utils/vote-helpers.php
+++ wp-voting-plugin/includes/utils/vote-helpers.php
@@ -7,3 +7,33 @@
  */
 
 defined('ABSPATH') || exit;
+
+
+function wpvp_format_vote_date($date) {
+    return 'Date stub';
+}
+
+function wpvp_get_vote_url($action, $vote_id) {
+    return '#';
+}
+
+function wpvp_get_vote_status_badge($status) {
+    return '<span>Status stub</span>';
+}
+
+function wpvp_get_vote_type_label($type) {
+    return 'Type stub';
+}
+
+function wpvp_can_edit_vote($vote) {
+    return true;
+}
+
+function wpvp_can_delete_vote($vote) {
+    return true;
+}
+
+function wpvp_can_view_results($vote) {
+    return true;
+}
+
