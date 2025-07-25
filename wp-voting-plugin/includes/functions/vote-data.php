3ffa0f1fb07b50fd31f758bcdd749811a846303c
Greg Hacke <greghacke@rskgroup.org>
2025-07-25 12:56:43 -0400

    Updates

--- wp-voting-plugin/includes/functions/vote-data.php
+++ wp-voting-plugin/includes/functions/vote-data.php
@@ -22,34 +22,6 @@ function wpvp_get_pagination_args() {
     );
 }
 
-function wpvp_format_vote_date($date) {
-    return 'Date stub';
-}
-
-function wpvp_get_vote_url($action, $vote_id) {
-    return '#';
-}
-
-function wpvp_get_vote_type_label($type) {
-    return 'Type stub';
-}
-
-function wpvp_get_vote_status_badge($status) {
-    return '<span>Status stub</span>';
-}
-
-function wpvp_can_edit_vote($vote) {
-    return true;
-}
-
-function wpvp_can_view_results($vote) {
-    return true;
-}
-
-function wpvp_can_delete_vote($vote) {
-    return true;
-}
-
 function wpvp_render_vote_actions($vote) {
     return '<div class="vote-actions">Actions stub</div>';
 }
